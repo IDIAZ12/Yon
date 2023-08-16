5dcb34effc898c7c7a171a090bdbcfd34ac6ce74
IDIAZ12 <142353257+IDIAZ12@users.noreply.github.com>
2023-08-16 13:24:55 -0700

    Add files via upload

---
+++ perfiles.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>PERFILES</title>
+</head>
+<body>
+    <H1>CATALOGO</H1>
+
+
+    <a href="index.php">MENU  </a><BR><BR>
+
+ID <input type="text" SIZE=5 name="ID_PERFILES"><BR><BR>
+PERFIL: <input type="text" name="PERFIL"><BR><BR>
+ÁREA: <input type="text" name="AREA"><BR><BR><BR>
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -<BR>
+GUARDAR / ACTUALIZAR / ELIMINAR / BUSCAR / REPORTE <BR>
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -<BR><BR>
+    <a href="perfiles.php">PERFILES / </a>
+    <a href="campus.php">CAMPUS /</a>
+    <a href="servidores.php">SERVIDORES /</a>
+    <a href="servo5.php">USUARIOS SERVO 5 /</a>
+    <a href="servoweb.php">USUARIOS SERVOWEB /</a>
+</body>
+</html>
