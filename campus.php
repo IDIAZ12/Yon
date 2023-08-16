6ed99935e5a833049497ae183d36795dbfd0d359
IDIAZ12 <142353257+IDIAZ12@users.noreply.github.com>
2023-08-16 13:56:25 -0700

    Add files via upload

---
+++ campus.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>CAMPUS</title>
+</head>
+<body>
+    <H1>CATALOGO</H1>
+
+
+    <a href="index.php">MENU  </a><BR><BR>
+
+ID <input type="text" SIZE=5 name="ID_CAMPUS"><BR><BR>
+CAMPUS: <input type="text" name="CAMPUS"><BR><BR>
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -<BR>
+GUARDAR / ACTUALIZAR / ELIMINAR / BUSCAR / REPORTE <BR>
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -<BR><BR>
+    <a href="perfiles.php">PERFILES / </a>
+    <a href="campus.php">CAMPUS /</a>
+    <a href="servidores.php">SERVIDORES /</a>
+    <a href="servo5.php">USUARIOS SERVO 5 /</a>
+    <a href="servoweb.php">USUARIOS SERVOWEB /</a>
+</body>
+</html>
---
+++ servidores.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>SERVIDORES</title>
+</head>
+<body>
+    <H1>CATALOGO</H1>
+
+
+    <a href="index.php">MENU  </a><BR><BR>
+
+ID <input type="text" SIZE=5 name="ID_SERVIDOR"><BR><BR>
+SERVIDOR: <input type="text" name="SERVIDOR"><BR><BR>
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -<BR>
+GUARDAR / ACTUALIZAR / ELIMINAR / BUSCAR / REPORTE <BR>
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -<BR><BR>
+    <a href="perfiles.php">PERFILES / </a>
+    <a href="campus.php">CAMPUS /</a>
+    <a href="servidores.php">SERVIDORES /</a>
+    <a href="servo5.php">USUARIOS SERVO 5 /</a>
+    <a href="servoweb.php">USUARIOS SERVOWEB /</a>
+</body>
+</html>
