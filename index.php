06515b306900a8d36ddc7b9098b04c739ee5dd18
IDIAZ12 <142353257+IDIAZ12@users.noreply.github.com>
2023-08-16 13:35:10 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>MENU</title>
+</head>
+<body>
+<title>CATALOGO</title>
+
+    <a href="perfiles.php">PERFILES <BR></a>
+    <a href="campus.php">CAMPUS<BR></a>
+    <a href="servidores.php">SERVIDORES<BR></a>
+    <a href="servo5.php">USUARIOS SERVO 5<BR></a>
+    <a href="servoweb.php">USUARIOS SERVOWEB <BR></a>
+</body>
+</html>
